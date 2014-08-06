cb9cedc450cc7bae6c5fcb2e4f738cd24a8eb349
great-tankard <meatloafthefrog@gmail.com>
2014-08-06 12:19:19 -0500

    reinitialized files

---
+++ content/pages/About.md
@@ -0,0 +1,8 @@
+Title: About
+
+
+I'm Abir Majumdar.  
+I have an email address: meatloafthefrog@gmail<i>.</i>com  
+I'm on Twitter:  [@great_tankard](https://twitter.com/great_tankard)  
+Come say hi. 
+
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Abir Majumdar'
+SITENAME = 'Abir Majumdar'
+SITETAGLINE = 'useless sack of cellular activity'
+SITEURL = 'http://enjoyabir.com'
+DEFAULT_CATEGORY = 'Blog'
+
+TIMEZONE = 'America/Chicago'
+DEFAULT_DATE_FORMAT = ('%Y-%m-%d')
+DEFAULT_LANG = 'en'
+
+
+
+
+THEME = '/home/abir/documents/pelican/enjoy/theme/EnjoyAbir'
+
+
+
+# Blogroll
+#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#          ('Python.org', 'http://python.org'),
+#          ('Jinja2', 'http://jinja.pocoo.org'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 3
